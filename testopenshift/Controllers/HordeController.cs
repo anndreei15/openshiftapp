7f6dd09ae96e93c4569652720d38905aadee3acb
Andrei Fekete <anndreei15@gmail.com>
2019-01-12 14:36:07 +0200

    Importing project

---
+++ testopenshift/Controllers/HordeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testopenshift.Controllers
+{
+    public class HordeController : Controller
+    {
+        private static Dictionary<string, string> _Students = new Dictionary<string, string>();
+
+        [HttpGet("horde/create/{student}")]
+        public string Post(string student, string prenume)
+        {
+            _Students[student] = prenume;
+
+            return String.Format("{0} added to the list!", student);
+        }
+
+        [HttpPatch("horde/edit/{student}")]
+        public string Patch(string student, string prenume)
+        {
+            if (!_Students.ContainsKey(student))
+            {
+                return "Student not found.";
+            }
+
+            _Students[student] = prenume;
+
+            return "Student edited.";
+        }
+
+        [HttpGet("horde/details/{student}")]
+        public string Details(string student)
+        {
+            if (!_Students.ContainsKey(student))
+            {
+                return "Student not found.";
+            }
+
+            return _Students[student];
+        }
+
+        [HttpDelete("horde/delete/{student}")]
+        public string Delete(string student)
+        {
+            if (!_Students.ContainsKey(student))
+            {
+                return "Student not found.";
+            }
+
+            _Students.Remove(student);
+
+            return "Student deleted.";
+        }
+
+
+    }
+}
---
+++ testopenshift/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testopenshift.Controllers
+{
+    public class StudentController
+    {
+        [HttpGet("student/{student}")]
+        public string Student(string student)
+        {
+            switch(student)
+            {
+                case "create":
+                    return "Create a new student";
+                case "edit":
+                    return "Edit student info!";
+                case "details":
+                    return "Student details below!";
+                case "delete":
+                    return "Are you sure you want to delete this student?";
+
+            }
+            return String.Empty;
+        }
+    }
+}
